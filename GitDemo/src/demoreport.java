c195b67e5b41825529831fbc7b6d965c0b7df25f
hp <hp@DESKTOP-0CQ6GA4>
2020-11-24 05:47:08 -0800

    create a variable i and print its contents

--- GitDemo/src/demoreport.java
+++ GitDemo/src/demoreport.java
@@ -3,6 +3,8 @@ public class demoreport {
 
 	public static void main(String[] args) {
 		System.out.print("Hi git i am here :");
+	    int i = 10;
+		System.out.print("Here is value :" + i);
 	}
 
 }
