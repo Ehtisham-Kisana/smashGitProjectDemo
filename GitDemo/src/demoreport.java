cb61606572d5dcd70df0b23a804b104913c492a3
Ehtisham Kisana <FA19-BCS-169@cuiatd.edu.pk>
2020-11-24 09:01:17 -0800

    i updated

--- GitDemo/src/demoreport.java
+++ GitDemo/src/demoreport.java
@@ -3,7 +3,7 @@ public class demoreport {
 
 	public static void main(String[] args) {
 		System.out.print("Hi git i am here :");
-	    int i = 20;
+	    int i = 330;
 		System.out.print("Here is value : " + i);
 	}
 
