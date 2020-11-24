14828823ecefd5e7fb57e8bce2d97c299aa6e283
Ehtisham Kisana <FA19-BCS-169@cuiatd.edu.pk>
2020-11-24 05:54:47 -0800

    create a variable

--- GitDemo/src/demoreport.java
+++ GitDemo/src/demoreport.java
@@ -4,7 +4,7 @@ public class demoreport {
 	public static void main(String[] args) {
 		System.out.print("Hi git i am here :");
 	    int i = 10;
-		System.out.print("Here is value :" + i);
+		System.out.print("Here is value : " + i);
 	}
 
 }
